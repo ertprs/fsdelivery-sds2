5b6a0a4f094020ab02be3ed9d55fa84268edbaf4
unknown <felipescherer25@gmail.com>
2021-01-09 14:04:22 -0300

    Finish Class 2

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,9 +1,25 @@
+import { useEffect, useState } from 'react';
+import { fetchProducts } from '../api';
+import ProductsList from './ProductsList';
 import StepHeader from './StepsHeader';
 import './style.css';
+import { Product } from './typess';
+
 
 function Orders(){
+
+    const [products, setProducts] = useState<Product[]>([]);
+
+    useEffect(() => {
+        fetchProducts().then(response => setProducts(response.data)).catch(error => console.log(error));
+    }, []);
+
     return (
-        <StepHeader />
+        <div className="orders-container">
+            <StepHeader />
+            <ProductsList products={products}/>
+        </div>
+
     )
 }
 
